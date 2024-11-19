67352118fd3695aca5b271d0985a7c4b5c67b015
bcx <femolak@gmail.com>
2024-11-19 18:39:50 +0100

    enums for license classification

---
+++ licesenser/enums.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class LicenseType(Enum):
+    MIT = "MIT License"
+    APACHE = "Apache License"
+    GPL = "GNU General Public License"
+    BSD = "BSD License"
+    ISC = "ISC License"
+    MPL = "Mozilla Public License"
+    EPL = "Eclipse Public License"
+    AGPL = "GNU Affero General Public License"
+    LGPL = "GNU Lesser General Public License"
+    UNLICENSE = "The Unlicense"
+    ZZ = "Zlib License"
+    ARTISTIC = "Artistic License"
+    CC0 = "CC0 1.0 Universal"
+    NONE = "NONE"
+    UNKNOWN = "UNKNOWN"
