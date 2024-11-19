548ff3c7d49f6b004dffae4ddb910c9b1fa982fc
bcx <femolak@gmail.com>
2024-11-19 18:59:41 +0100

    conftests updates with new mock data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,16 @@
 import os
+import tempfile
+from typing import Generator
 
 import pytest  # type: ignore
+
+from licesenser.license_manager.get_project_license import (FileFinder,
+                                                            LicenseFinder)
+from tests.data.mock_data import (empty_license, invalid_file_content,
+                                  invalid_licenses,
+                                  mock_directory_invalid_structure,
+                                  mock_directory_valid_structure,
+                                  valid_licenses)
 
 
 @pytest.fixture(scope="module")
@@ -21,3 +31,59 @@ def requirements_data() -> str:
 @pytest.fixture(scope="module")
 def unsupported_data() -> str:
     return os.path.join(os.path.dirname(__file__), "data", "dependencies.json")
+
+
+# Test setup
+@pytest.fixture
+def root_directory() -> str:
+    return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+@pytest.fixture
+def file_finder() -> FileFinder:
+    return FileFinder()
+
+
+@pytest.fixture
+def license_finder(file_finder: FileFinder) -> LicenseFinder:
+    return LicenseFinder(file_finder)
+
+
+@pytest.fixture
+def root_directory_valid() -> Generator[str, None, None]:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        for file_name, content in mock_directory_valid_structure.items():
+            file_path = os.path.join(temp_dir, file_name)
+            with open(file_path, "w") as f:
+                f.write(content)
+        yield temp_dir
+
+
+@pytest.fixture
+def root_directory_invalid() -> Generator[str, None, None]:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        for file_name, content in mock_directory_invalid_structure.items():
+            file_path = os.path.join(temp_dir, file_name)
+            with open(file_path, "w") as f:
+                f.write(content)
+        yield temp_dir
+
+
+@pytest.fixture
+def known_license_text() -> list[str]:
+    return valid_licenses
+
+
+@pytest.fixture
+def unknown_license_text() -> list[str]:
+    return invalid_licenses
+
+
+@pytest.fixture
+def empty_license_text() -> list[str]:
+    return empty_license
+
+
+@pytest.fixture
+def invalid_file_text() -> list[str]:
+    return invalid_file_content
